653f89cbc9da06d67af09964203a5fbc02c4c5cc
mousami chand <ermousamichand@gmail.com>
2018-08-03 11:05:33 +0200

    author and genre listing

---
+++ CoolBooks/Controllers/AuthorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CoolBooks.Models;
+
+namespace CoolBooks.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private CoolBooksEntities db = new CoolBooksEntities();
+
+        // GET: Authors
+        public ActionResult Index()
+        {
+            return View(db.Authors.ToList());
+        }
+
+        // GET: Authors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Authors authors = db.Authors.Find(id);
+            if (authors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(authors);
+        }
+
+        // GET: Authors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Authors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Description,Created,IsDeleted")] Authors authors)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Authors.Add(authors);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(authors);
+        }
+
+        // GET: Authors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Authors authors = db.Authors.Find(id);
+            if (authors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(authors);
+        }
+
+        // POST: Authors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Description,Created,IsDeleted")] Authors authors)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(authors).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(authors);
+        }
+
+        // GET: Authors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Authors authors = db.Authors.Find(id);
+            if (authors == null)
+            {
+                return HttpNotFound();
+            }
+            return View(authors);
+        }
+
+        // POST: Authors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Authors authors = db.Authors.Find(id);
+            db.Authors.Remove(authors);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
