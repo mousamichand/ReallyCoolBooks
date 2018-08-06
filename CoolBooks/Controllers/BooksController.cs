92c0561fae95805d2802796722c159ed60a71727
Jonas Stensiö <jonas@stensio.net>
2018-08-06 14:41:02 +0200

    ISBN stuf

--- CoolBooks/Controllers/BooksController.cs
+++ CoolBooks/Controllers/BooksController.cs
@@ -15,7 +15,7 @@ using CoolBooks.ViewModels;
 
 namespace CoolBooks.Controllers
 {
-    public class BooksController : Controller
+    public class BooksController: Controller
     {
         private CoolBooksEntities db = new CoolBooksEntities();
 
@@ -36,15 +36,15 @@ namespace CoolBooks.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-         //   Books books = db.Books.Find(id);
+            //   Books books = db.Books.Find(id);
 
             var abc = from b in db.Books
                       join r in db.Reviews on b.Id equals r.BookId
                       where b.Id == id
-                      select new { b.Id, b.Title, b.Description, b.ImagePath,b.Created,b.Authors,b.Genres, r.Text, r.UserId };
+                      select new { b.Id, b.Title, b.Description, b.ImagePath, b.Created, b.Authors, b.Genres, r.Text, r.UserId };
 
 
-            
+
             if (abc == null)
             {
                 return HttpNotFound();
@@ -135,6 +135,106 @@ namespace CoolBooks.Controllers
 
         //}
 
+        public int GetGenreByName(string name)
+        {
+            List<Genres> genres = db.Genres.ToList<Genres>();
+            foreach (Genres genre in genres)
+                if (genre.Name == name) return genre.Id;
+            Genres model = db.Genres.Create();
+            model.Name = name;
+            model.Created = DateTime.Now;
+            model.IsDeleted = false;
+            db.Genres.Add(model);
+            db.SaveChanges();
+            return model.Id;
+        }
+
+        public int GetAuthorByName(string name)
+        {
+            name = name.Trim();
+            string[] names = name.Split();
+            string lastName = names.Last<string>();
+            string fstName = "";
+            for(int i = 0; i < names.Length-1; ++i)
+                fstName += names[i] + " ";
+            fstName = fstName.Trim();
+            List<Authors> authors = db.Authors.ToList<Authors>();
+            foreach (Authors author in authors)
+                if ((author.FirstName.ToLower() == fstName.ToLower()) 
+                    && (author.LastName.ToLower() == lastName.ToLower()))
+                    return author.Id;
+            Authors model = db.Authors.Create();
+            model.FirstName = fstName;
+            model.LastName = lastName;
+            model.Created = DateTime.Now;
+            model.IsDeleted = false;
+            db.Authors.Add(model);
+            db.SaveChanges();
+            return model.Id;
+        }
+
+        /*
+    books.Created = DateTime.Now;
+    books.IsDeleted = false;
+    db.Books.Add(books);
+    db.SaveChanges();
+    return RedirectToAction("Index");
+    }
+    else
+    {
+       // Item  book = GoogleBooksAPI.SearchBook(books.ISBN);
+
+       //ViewBag.booktitle = book.VolumeInfo.Title;
+
+        ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", books.UserId);
+        ViewBag.AuthorId = new SelectList(db.Authors, "Id", "FirstName", books.AuthorId);
+        ViewBag.GenreId = new SelectList(db.Genres, "Id", "Name", books.GenreId);
+
+
+    //        Books book = new Books();
+    //        book.GenreId = 1 ;
+    //        book.AuthorId = 1;
+    //    book.Title = "crazy .net mvc";
+    //return View(book);
+
+
+       Books objbook = new Books
+        {
+            Id = 1,
+            AuthorId=1,
+            UserId="mchand",
+            ISBN="1238",
+            Title = "Priti kumari",
+           Created = DateTime.Now
+
+        };
+
+        return View("create",objbook);
+    }
+
+}
+
+else
+    return View(books);
+
+    }
+    return Index();
+}
+    */
+
+        //[HttpPost]
+        //public void autofill([Bind(Include = "ISBN")] Books books)
+        //{
+
+        //        GoogleBooksAPI.SearchBook(books.ISBN);
+        //        books.GenreId = 1;
+        //        books.Created = DateTime.Now;
+        //        books.IsDeleted = false;
+
+
+
+
+        //}
 
         // GET: Books/Edit/5
         public ActionResult Edit(int? id)
@@ -207,11 +307,11 @@ namespace CoolBooks.Controllers
         {
 
             var booksReview = (from b in db.Books
-                      join r in db.Reviews on b.Id equals r.BookId
-                      where b.Id == id
-                      select new { b.Id, b.Title, b.Description,b.ImagePath,r.Text,r.UserId }).ToList();
+                               join r in db.Reviews on b.Id equals r.BookId
+                               where b.Id == id
+                               select new { b.Id, b.Title, b.Description, b.ImagePath, r.Text, r.UserId }).ToList();
 
-               return View(booksReview);
+            return View(booksReview);
         }
 
 
@@ -242,7 +342,7 @@ namespace CoolBooks.Controllers
     }
 }
 
-    
+
 
 
 
