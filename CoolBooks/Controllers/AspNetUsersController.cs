7d0b4a7be99173caaa224f56f9f702ee3c626943
Jonas Stensiö <jonas@stensio.net>
2018-08-07 13:25:32 +0200

    SignUp

--- CoolBooks/Controllers/AspNetUsersController.cs
+++ CoolBooks/Controllers/AspNetUsersController.cs
@@ -92,13 +92,25 @@ namespace CoolBooks.Controllers
                 ViewBag.ErrMessage = "Password must be filled in";
                 hasErrors = true;
             }
-            if (userName )
-
+            if (aspNetUsers.UserName.Trim() == "")
+            {
+                ViewBag.ErrMessage = "Password must be filled in";
+                hasErrors = true;
+            }
+            int count = (from i in db.AspNetUsers
+                         where (i.UserName == aspNetUsers.UserName)
+                         select i).Count();
+            if (0 < count)
+            {
+                ViewBag.ErrMessage = "User name already exists";
+                hasErrors = true;
+            }
 
             if (hasErrors)
             {
                 return View();
-            } else { 
+            } else
+            {
                 aspNetUsers.Id = Guid.NewGuid().ToString();
                 aspNetUsers.EmailConfirmed = true;
                 aspNetUsers.PasswordHash = GetPasswordHash(password);
@@ -112,10 +124,10 @@ namespace CoolBooks.Controllers
                 db.AspNetUsers.Add(aspNetUsers);
                 db.SaveChanges();
                 return RedirectToAction("../Users/Profile");
-            } 
+            }
         }
 
-        // GET: AspNetUsers/Edit/5
+        // GET: AspNetUsers/Edit
         public ActionResult Edit(string id)
         {
             if (id == null)
@@ -130,7 +142,6 @@ namespace CoolBooks.Controllers
             ViewBag.Id = new SelectList(db.Users, "UserId", "FirstName", aspNetUsers.Id);
             return View(aspNetUsers);
         }
-
 
         // POST: AspNetUsers/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
@@ -175,7 +186,7 @@ namespace CoolBooks.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-
+*/
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -185,111 +196,7 @@ namespace CoolBooks.Controllers
             base.Dispose(disposing);
         }
     }
-
-    public sealed class SecurePasswordHasher
-    {
-        /// <summary>
-        /// Size of salt
-        /// </summary>
-        private const int SaltSize = 16;
-
-        /// <summary>
-        /// Size of hash
-        /// </summary>
-        private const int HashSize = 20;
-
-        /// <summary>
-        /// Creates a hash from a password
-        /// </summary>
-        /// <param name="password">the password</param>
-        /// <param name="iterations">number of iterations</param>
-        /// <returns>the hash</returns>
-        /*
-        public static string Hash(string password, int iterations)
-        {
-            //create salt
-            byte[] salt;
-            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltSize]);
-
-            //create hash
-            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
-            var hash = pbkdf2.GetBytes(HashSize);
-
-            //combine salt and hash
-            var hashBytes = new byte[SaltSize + HashSize];
-            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
-            Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
-
-            //convert to base64
-            var base64Hash = Convert.ToBase64String(hashBytes);
-
-            //format hash with extra information
-            return string.Format("$MYHASH$V1${0}${1}", iterations, base64Hash);
-        }
-        /// <summary>
-        /// Creates a hash from a password with 10000 iterations
-        /// </summary>
-        /// <param name="password">the password</param>
-        /// <returns>the hash</returns>
-        public static string Hash(string password)
-        {
-            return Hash(password, 10000);
-        }
-
-        /// <summary>
-        /// Check if hash is supported
-        /// </summary>
-        /// <param name="hashString">the hash</param>
-        /// <returns>is supported?</returns>
-        public static bool IsHashSupported(string hashString)
-        {
-            return hashString.Contains("$MYHASH$V1$");
-        }
-
-        /// <summary>
-        /// verify a password against a hash
-        /// </summary>
-        /// <param name="password">the password</param>
-        /// <param name="hashedPassword">the hash</param>
-        /// <returns>could be verified?</returns>
-        public static bool Verify(string password, string hashedPassword)
-        {
-            //check hash
-            if (!IsHashSupported(hashedPassword))
-            {
-                throw new NotSupportedException("The hashtype is not supported");
-            }
-
-            //extract iteration and Base64 string
-            var splittedHashString = hashedPassword.Replace("$MYHASH$V1$", "").Split('$');
-            var iterations = int.Parse(splittedHashString[0]);
-            var base64Hash = splittedHashString[1];
-
-            //get hashbytes
-            var hashBytes = Convert.FromBase64String(base64Hash);
-
-            //get salt
-            var salt = new byte[SaltSize];
-            Array.Copy(hashBytes, 0, salt, 0, SaltSize);
-
-            //create hash with given salt
-            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
-            byte[] hash = pbkdf2.GetBytes(HashSize);
-
-            //get result
-            for (var i = 0; i < HashSize; i++)
-            {
-                if (hashBytes[i + SaltSize] != hash[i])
-                {
-                    return false;
-                }
-            }
-            return true;
-
-
-        }
-        */
-    }
-
-
 }
+
+
+
