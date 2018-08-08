854ed58b5c009efcde36968c82d7096b32a7f7b6
wael henawe <wael.henawe@outlook.com>
2018-08-08 11:12:32 +0200

    Search Page

--- CoolBooks/App_Start/RouteConfig.cs
+++ CoolBooks/App_Start/RouteConfig.cs
@@ -14,9 +14,10 @@ namespace CoolBooks
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute(name: "Default", url: "{controller}/{action}/{id}", defaults: new
+            routes.MapRoute(name: "Home", url: "{controller}/{action}/{id}", 
+                defaults: new
             {
-                controller = "Captcha",
+                controller = "Home",
                 action = "Index",
                 id = UrlParameter.Optional
             });
